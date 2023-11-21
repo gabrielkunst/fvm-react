8a110c810852be663ab5b37384accf5f66010acb
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-11-20 21:44:01 -0300

    feat: adding assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"white-custom": "#ffffff",
+				"black-custom": "#000000",
+				"text-white": "#ffffff",
+				"text-black": "#000000",
+				"text-gray": "#999999",
+				primary: "#2552a7",
+				secondary: "#4480ef",
+				"neutral-100": "#f5f6f7",
+				"neutral-200": "#e7e7e7",
+				"neutral-300": "#3c4453",
+			},
+			gridTemplateColumns: {
+				responsive: "repeat(auto-fit, minmax(250px, auto))",
+			},
+		},
 	},
 	plugins: [],
 };
